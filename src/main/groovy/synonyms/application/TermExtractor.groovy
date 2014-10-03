ed63f5cd347222a0164833f0907e125e8255d72a
Piotr Betkier <pbetkier@gmail.com>
2014-10-03 22:05:50 +0200

    makes the plugin cross-platform compliant (e.g. PyCharm).

--- src/main/groovy/synonyms/application/TermExtractor.groovy
+++ src/main/groovy/synonyms/application/TermExtractor.groovy
@@ -3,14 +3,13 @@ package synonyms.application
 import com.google.common.base.Optional
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiIdentifier
 import com.intellij.spellchecker.inspections.IdentifierSplitter
 import synonyms.domain.Term
 
 class TermExtractor {
 
     Optional<Term> fromElement(PsiElement verified, int cursorPosition) {
-        if (verified instanceof PsiIdentifier) {
+        if (verified.text) {
             List<TextRange> ranges = []
             new IdentifierSplitter().split(verified.text, new TextRange(0, verified.text.size()), {
                 ranges.add(it)
--- src/test/groovy/synonyms/application/TermExtractorSpec.groovy
+++ src/test/groovy/synonyms/application/TermExtractorSpec.groovy
@@ -1,7 +1,6 @@
 package synonyms.application
 
-import com.intellij.psi.PsiComment
-import com.intellij.psi.impl.source.tree.java.PsiIdentifierImpl
+import com.intellij.psi.PsiElement
 import spock.lang.Specification
 import spock.lang.Unroll
 import synonyms.domain.Term
@@ -10,14 +9,19 @@ class TermExtractorSpec extends Specification {
 
     def extractor = new TermExtractor()
 
-    def "should not extract term for element which is not an identifier"() {
+    def "should not extract term for element which doesn't contain any text"() {
         expect:
-        !extractor.fromElement(Stub(PsiComment), 0).isPresent()
+        !extractor.fromElement(Stub(PsiElement), 0).isPresent()
     }
 
-    def "should extract term from a simple identifier"() {
+    def "should extract term from a simple element with text"() {
+        given:
+        def simpleElement = Stub(PsiElement) {
+            getText() >> "term"
+        }
+
         when:
-        def extracted = extractor.fromElement(new PsiIdentifierImpl("term"), 0)
+        def extracted = extractor.fromElement(simpleElement, 0)
 
         then:
         extracted.isPresent()
@@ -25,16 +29,21 @@ class TermExtractorSpec extends Specification {
     }
 
     @Unroll
-    def "should extract term #expected from complex identifier #identifier at #position"() {
+    def "should extract term #expected from complex element with #text at #position"() {
+        given:
+        def complexElement = Stub(PsiElement) {
+            getText() >> text
+        }
+
         when:
-        def extracted = extractor.fromElement(new PsiIdentifierImpl(identifier), position)
+        def extracted = extractor.fromElement(complexElement, position)
 
         then:
         extracted.isPresent()
         extracted.get() == new Term(expected)
 
         where:
-        identifier           | position                          || expected
+        text                 | position                          || expected
         "wordInsideSentence" | "wordInsideSentence".indexOf('I') || "inside"
         "wordInsideSentence" | "wordInsideSentence".indexOf('s') || "inside"
         "firstWord"          | "firstWord".indexOf('f')          || "first"
@@ -44,9 +53,14 @@ class TermExtractorSpec extends Specification {
         "wiThShoRtWords"     | "wiThShoRtWords".indexOf('d')     || "words"
     }
 
-    def "doesn't extract short terms"() {
+    def "doesn't extract terms shorter than 4 characters"() {
+        given:
+        def shortElement = Stub(PsiElement) {
+            getText() >> "top"
+        }
+
         when:
-        def extracted = extractor.fromElement(new PsiIdentifierImpl("top"), 0)
+        def extracted = extractor.fromElement(shortElement, 0)
 
         then:
         !extracted.isPresent()
