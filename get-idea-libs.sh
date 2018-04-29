9a23c66a3680ba3af50dbe4f320ff7ee7fa3cd70
Piotr Betkier <pbetkier@gmail.com>
2018-04-29 21:14:18 +0200

    changes travis build to use intellij sdk from jetbrains plugin

--- get-idea-libs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-IDEA_VERSION="2018.1.2"
-
-wget -O idea.tar.gz http://download-cf.jetbrains.com/idea/ideaIC-${IDEA_VERSION}.tar.gz
-
-tar xf idea.tar.gz
-ln -sT `find . -name 'idea-IC*'`/lib idea-libs
